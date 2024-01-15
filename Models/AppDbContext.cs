576bfff3ef9322330ef18239113efd0067cf542c
Mubina Waheed <mubina@globalresourcesca.com>
2024-01-15 20:51:43 +0500

    added a dbcontext class

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace employeeManagement.Models
+
+{
+	public class AppDbContext : DbContext
+	{
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) 
+        {
+            
+        }
+        //we will use this property to query and save instances of the employee class
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
