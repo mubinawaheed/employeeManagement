b0ea924a630b5c6d75d8bc91f39c5186099beef5
Mubina Waheed <mubina@globalresourcesca.com>
2024-01-20 23:07:21 +0500

    Update employee function complete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using employeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Security.AccessControl;
 
 
@@ -20,7 +20,6 @@ namespace employeeManagement.Controllers
         {
             _employeeRepository = employeeRepository;
 			_webHostEnvironment = env;
-
 		}
  
 		public ViewResult Index()
@@ -50,18 +49,7 @@ namespace employeeManagement.Controllers
         {
             if (ModelState.IsValid)
             {
-                string? fileName = null;
-                if(model.ProfileImage!= null && model.ProfileImage.Count>0)
-                {
-                    foreach (IFormFile photo in model.ProfileImage)
-                    {
-                        string folderName = Path.Combine(_webHostEnvironment.WebRootPath, "images");
-                        fileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
-                        string combinedPath = Path.Combine(folderName, fileName);
-                        photo.CopyTo(new FileStream(combinedPath, FileMode.Create));
-                        
-                    }
-                }
+                string? fileName = ProcessUploadedFile(model);
                 Employee employee = new Employee{
                     Name = model.Name,
                     Email = model.Email,
@@ -95,5 +83,55 @@ namespace employeeManagement.Controllers
             };
             return View(employeeEditView);
         }
+
+        [HttpPost]
+		public IActionResult Edit(EmployeeEditViewModel model) //both viewResult and redirectToaction result implement this inteface IActionResult
+		{
+			if (ModelState.IsValid)
+			{
+				Employee employee = _employeeRepository.GetEmployee(model.Id); //this id is coming from the hidden input field in edit view file
+			    employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+
+                if(model.ProfileImage != null)
+                {
+				    employee.ProfileImage = ProcessUploadedFile(model);
+
+                }
+				_employeeRepository.Update(employee);
+                if (model.ExistingPhotoPath!=null)
+                {
+                    string filepath=Path.Combine(_webHostEnvironment.WebRootPath, "images", model.ExistingPhotoPath);
+                    System.IO.File.Delete(filepath);
+
+				}
+				return RedirectToAction("index");
+			}
+			return View();
+
+		}
+
+		private string? ProcessUploadedFile(EmployeeCreateViewModel model)
+		{
+			string? fileName = null;
+			if (model.ProfileImage != null && model.ProfileImage.Count > 0)
+			{
+				foreach (IFormFile photo in model.ProfileImage)
+				{
+					string folderName = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+					fileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+					string combinedPath = Path.Combine(folderName, fileName);
+                    using(var fileStream= new FileStream(combinedPath, FileMode.Create))
+                    {
+                        photo.CopyTo(fileStream);
+                    }
+					
+
+				}
+			}
+
+			return fileName;
+		}
 	}
 }
