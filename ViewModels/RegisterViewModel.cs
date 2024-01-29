a7bb1699bc0c5e57c87ce597149dbd2ad0762510
Mubina Waheed <mubina@globalresourcesca.com>
2024-01-29 23:07:40 +0500

    Added Register method

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace employeeManagement.ViewModels
+{
+	public class RegisterViewModel
+	{
+        [Required]
+		[EmailAddress]
+        public string? Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm Password")]
+		[Compare("Password", ErrorMessage ="Password does not match")]
+		public string? ConfirmPassword { get; set; }
+
+	}
+}
