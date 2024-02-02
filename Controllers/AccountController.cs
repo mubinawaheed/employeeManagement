e0b0f06273017df0b79c51fab622b8e6075d9d14
Mubina Waheed <mubina@globalresourcesca.com>
2024-02-03 00:07:45 +0500

    Created a function to register user in aspNetUsers table

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using employeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace employeeManagement.Controllers
 {
 	public class AccountController: Controller
 	{
-		[HttpGet]
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        [HttpGet]
 		public IActionResult Register()
 		{
 			return View();
 		}
-	}
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result= await userManager.CreateAsync(user, password: model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false); //This bool param is used to define whether we want to create a session cookie or a permanent cookie
+                    return RedirectToAction("index", "home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
 }
